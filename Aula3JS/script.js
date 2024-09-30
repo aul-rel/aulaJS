ea14a0fcdef5fc41eb01ae88eeb13b8a6f036698
aul-rel <aurellio@outlook.com.br>
2024-09-30 14:44:18 -0300

    Add files via upload

---
+++ Aula3JS/script.js
@@ -0,0 +1,19 @@
+//alert("ALERTA!"); //Alerta em cima da página
+
+//document.write("<h2>Bom dia</h2>");
+//console.log("Onde aparece?"); // escreve no console, geralmente usado para testes
+
+document.getElementById("dv1").innerHTML="<h6><mark>Mudou</mark></h6>"; //Busca elemento ID na página HTML e modifica e elemento
+
+document.querySelector("h1").innerHTML="Javascript"; //Seleciona elemento na página HTML e modifica elemento 
+
+document.querySelector("h2").innerHTML="<h6>JS</h6>"; //Seleciona elemento na página HTML e modifica elemento 
+
+document.querySelector("p").innerHTML="Mudou"; //
+
+document.querySelector("h1").style.color="darkred";
+
+document.querySelector("body").style.backgroundColor="lightgray";
+
+
+
